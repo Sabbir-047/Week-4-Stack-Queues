c57ec298732a4cf65e130bececc5a9862392b698
Sabbir-047 <sabbirbhuiyan026@gmail.com>
2025-05-03 19:37:07 +0600

    tried a new problem

---
+++ M-14-Queue-Implementation-And-STL/6-Implement-Stack-Using-Queues.cpp
@@ -0,0 +1,52 @@
+/*
+225. Implement Stack using Queues
+----------------------------------
+link -> https://leetcode.com/problems/implement-stack-using-queues/description/
+*/
+
+
+
+// -------- code ----------
+
+
+
+
+/*
+
+class MyStack
+{
+public:
+    queue<int> q;
+    MyStack() {}
+
+    void push(int x) { q.push(x); }
+
+    int pop()
+    {
+        queue<int> q1;
+        int val;
+        while (!q.empty())
+        {
+            val = q.front();
+            q.pop();
+            if (q.empty() == true)
+                break;
+            q1.push(val);
+        }
+        q = q1;
+        return val;
+    }
+
+    int top()
+    {
+        return q.back();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
+
+
+*/
