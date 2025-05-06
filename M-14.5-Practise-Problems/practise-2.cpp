09111d955afe0af954c26f9ac2d017fa966771b5
Sabbir-047 <sabbirbhuiyan026@gmail.com>
2025-05-06 10:18:56 +0600

    Solved problem -2

--- M-14.5-Practise-Problems/practise-2.cpp
+++ M-14.5-Practise-Problems/practise-2.cpp
@@ -10,33 +10,51 @@ int main()
     int n;
     cin >> n;
     // taking stack values
-    while(n--){
+    while (n--)
+    {
         int x;
         cin >> x;
         st.push(x);
     }
 
-
     // queue operation
     int m;
     cin >> m;
     // taking queue values
-    while(m--){
+    while (m--)
+    {
         int y;
         cin >> y;
         q.push(y);
     }
 
-    
+    // setting flag
+    int flag = 0;
+
     // conditions for the problem
-    while (!st.empty() || !q.empty())
+    while (!st.empty() && !q.empty())
     {
         if (st.top() == q.front())
-            cout << "YES" << endl;
+        {
+            flag = 1;
+            st.pop();
+            q.pop();
+        }
         else
+        {
             cout << "NO\n";
-            break;
+            return 0;
+        }
+    }
+
+    // conditions for the problem
+    if (flag == 1)
+        cout << "YES" << endl;
+    else
+    {
+        cout << "NO" << endl;
     }
     
+
     return 0;
 }
